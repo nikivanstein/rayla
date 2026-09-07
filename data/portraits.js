4beeb11cd7a263f76c504b0b6781a0d6bbdd16ed
Niki van Stein <n.van.stein@liacs.leidenuniv.nl>
2026-09-07 19:27:10 +0000

    new portraits

--- data/portraits.js
+++ data/portraits.js
@@ -13,6 +13,12 @@ const PORTRAIT_DATA = [
   {
     src: "img/rayla-portrait.jpg",
     alt: "Rayla, a star elf druid in her star-embroidered robe, raising a hand toward a glowing frog and drifting leaves in a mossy forest, beside her lion companion Rayla's Bait",
-    caption: "Field record",
+    caption: "Star-Elf Druid",
   },
+  { src: "img/portrait-travel.jpg", alt: "Rayla in a weathered traveling cloak on a rainy road", caption: "On the road" },
+  { src: "img/portrait-rogue.jpg", alt: "Rayla in a black cloak", caption: "As rogue" },
+  { src: "img/portrait-scrying.jpg", alt: "Rayla performing Scry", caption: "Scrying" },
+  { src: "img/portrait-winter.jpg", alt: "Rayla in winter clothes", caption: "In winter" },
+  { src: "img/portrait-rich.jpg", alt: "Rayla in rich clothes", caption: "As lady" },
+  { src: "img/portrait-old.jpg", alt: "Rayla as old human", caption: "As old human" },
 ];
